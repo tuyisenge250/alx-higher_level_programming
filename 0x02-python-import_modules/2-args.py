ea5316e92d6a355105fd8d081aea7921b50c61c3
tuyisenge250 <benjaminwell250@gmail.com>
2023-11-30 13:25:55 -0800

    benfun

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
     import sys
 
     count = len(sys.argv) - 1
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python3
+!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print the addition of all arguments."""
     import sys
-    count = len(sys.argv) - 1    
+
     total = 0
-    for i in range(count):
+    for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
     print("{}".format(total))
