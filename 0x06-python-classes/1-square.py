5a8f51727fa43965676540f17d72792de944024f
tuyisenge250 <benjaminwell250@gmail.com>
2023-12-19 12:06:40 -0800

    programming my hobb

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Define a class square."""
+
+class Square:
+    """represent a square."""
+    def __init__(self,size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+this is higher level programming python wi gone to discuss about it's awesome a good thing and big called class this is 0xo6-python-classes
