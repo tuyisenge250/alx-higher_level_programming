22f9a61856e35a7b4c9a2d287ad4e33c506d7703
tuyisenge250 <benjamin@DESKTOP-9HU6DH7>
2024-03-12 23:10:21 +0200

    update

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,21 @@
-class Rectangle{
-    constructor(w,h){
-        if((w > 0) && (h > 0)){
-            this.width = w;
-            this.height = h
-        }
+#!/usr/bin/node
+class Rectangle {
+    constructor (w, h) {
+      if ((w > 0) && (h > 0)) {
+        this.width = w;
+        this.height = h;
+      }
     }
-    print(){
-        let i,j;
-        for(i =0; i < this.height; i++){
-            let result = "";
-            for(j = 0; j < this.width; j++){
-                result += "X";
-            }
-            console.log(result);
+  
+    print () {
+      for (let i = 0; i < this.height; i++) {
+        let s = '';
+        for (let j = 0; j < this.width; j++) {
+          s += 'X';
         }
+        console.log(s);
+      }
     }
-}
-module.exports = Rectangle
+  }
+  
+  module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,28 +1,32 @@
-class Rectangle{
-    constructor(w,h){
-        if((w > 0) && (h > 0)){
-            this.width = w;
-            this.height = h
+#!/usr/bin/node
+class Rectangle {
+    constructor (w, h) {
+      if ((w > 0) && (h > 0)) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  
+    print () {
+      for (let i = 0; i < this.height; i++) {
+        let s = '';
+        for (let j = 0; j < this.width; j++) {
+          s += 'X';
         }
+        console.log(s);
+      }
     }
-    print(){
-        let i,j;
-        for(i =0; i < this.height; i++){
-            let result = "";
-            for(j = 0; j < this.width; j++){
-                result += "X";
-            }
-            console.log(result);
-        }
+  
+    rotate () {
+      const aux = this.width;
+      this.width = this.height;
+      this.height = aux;
     }
-    rotate(){
-        let tmp = this.height;
-        this.height = this.width;
-        this.width = tmp;
+  
+    double () {
+      this.width *= 2;
+      this.height *= 2;
     }
-    double(){
-        this.height *= 2;
-        this.width *= 2;
-    }
-}
-module.exports = Rectangle
+  }
+  
+  module.exports = Rectangle;
